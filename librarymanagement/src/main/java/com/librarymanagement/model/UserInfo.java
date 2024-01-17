f7a4ed14181f2c82df23d596763a4f6b2dbe1ead
AjayBalu2002 <113227501+AjayBalu2002@users.noreply.github.com>
2024-01-17 12:01:10 +0530

    Create UserInfo.java

---
+++ librarymanagement/src/main/java/com/librarymanagement/model/UserInfo.java
@@ -0,0 +1,58 @@
+package com.librarymanagement.model;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+@Entity
+public class UserInfo {
+
+	
+	
+	public long getId() {
+		return id;
+	}
+
+	public void setId(long id) {
+		this.id = id;
+	}
+
+	public String getUserName() {
+		return UserName;
+	}
+
+	public void setUserName(String userName) {
+		UserName = userName;
+	}
+
+	public String getPassword() {
+		return Password;
+	}
+
+	public void setPassword(String password) {
+		Password = password;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	public long id;
+	
+	
+	public String UserName;
+	
+	public String Password;
+	
+	public String email;
+	
+	
+	
+}
