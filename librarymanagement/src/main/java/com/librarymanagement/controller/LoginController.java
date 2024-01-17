dc398c996f2e7d55983c913f565ff0d731d28cda
AjayBalu2002 <113227501+AjayBalu2002@users.noreply.github.com>
2024-01-17 12:00:27 +0530

    Create LoginController.java

---
+++ librarymanagement/src/main/java/com/librarymanagement/controller/LoginController.java
@@ -0,0 +1,40 @@
+package com.librarymanagement.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.librarymanagement.model.UserInfo;
+import com.librarymanagement.service.LoginService;
+
+@RestController
+@RequestMapping("/login")
+public class LoginController {
+
+	@Autowired
+	public LoginService service;
+	
+	@PostMapping("/register")
+	public ResponseEntity<String> registration(@RequestBody UserInfo user)
+	{
+	return	service.Register( user );
+		
+		
+	}
+	
+	
+	@PostMapping("/login")
+	public ResponseEntity<String> login(@RequestBody UserInfo user)
+	{
+		return service.login(user);
+	}
+	
+	
+
+	
+	
+}
