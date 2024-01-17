854770b5a8dbc5207e3b75dcfbe6b65785a69db2
AjayBalu2002 <113227501+AjayBalu2002@users.noreply.github.com>
2024-01-17 12:03:23 +0530

    Create LoginService.java

---
+++ librarymanagement/src/main/java/com/librarymanagement/service/LoginService.java
@@ -0,0 +1,56 @@
+package com.librarymanagement.service;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Service;
+
+import com.librarymanagement.model.Book;
+import com.librarymanagement.model.UserInfo;
+import com.librarymanagement.repository.LoginRepo;
+
+@Service
+public class LoginService {
+
+	@Autowired
+	public LoginRepo repo;
+	
+	public ResponseEntity<String> Register(UserInfo user)
+	{
+	
+		if(repo.findByEmail(user.getEmail()) != null)
+		{
+			
+			return ResponseEntity.badRequest().body("email already existed");
+			
+		
+			
+		}
+		else
+		{
+		
+			repo.save(user);
+			
+			return ResponseEntity.ok("registration success");	
+			
+		}
+		
+	}
+	
+	
+	
+	public ResponseEntity<String> login(UserInfo user)
+	{
+		UserInfo tempInfo = repo.findByEmail(user.email);
+		if(tempInfo != null && tempInfo.getPassword().equals(user.getPassword()) )
+		{
+			return ResponseEntity.ok("login success");	
+		}
+		else
+		{
+			return ResponseEntity.badRequest().body("username or password is wrong");		
+		}
+		
+	}
+	
+	
+}
